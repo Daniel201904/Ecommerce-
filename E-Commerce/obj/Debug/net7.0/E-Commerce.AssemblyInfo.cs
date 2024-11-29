68fb57914bc5f0ca561d6dc781bad3f455d81dfe
Daniel201904 <danivar763@gmail.com>
2024-11-29 17:35:42 -0500

    .

--- E-Commerce/obj/Debug/net7.0/E-Commerce.AssemblyInfo.cs
+++ E-Commerce/obj/Debug/net7.0/E-Commerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b752f389224162fae4629589b4e8105cf7d3247")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cb9758624dd85b9ac527e9a6b7a274783cf6007")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
