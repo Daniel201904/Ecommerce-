bb2f86b93921a336b70f6e3c013bcd3e6a58e4d2
Daniel201904 <danivar763@gmail.com>
2024-11-29 16:07:10 -0500

    Se hace el commit del scritp de la base de datos

--- Ecommerce/E-Commerce/obj/Debug/net7.0/E-Commerce.AssemblyInfo.cs
+++ Ecommerce/E-Commerce/obj/Debug/net7.0/E-Commerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60c66b5ffbb3d2e9da8479d89a4b2ad22206c8e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44294a62128a6428f4a942ac9b1947fc12f93fcc")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
