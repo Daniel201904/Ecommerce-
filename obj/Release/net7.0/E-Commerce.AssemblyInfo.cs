1cb9758624dd85b9ac527e9a6b7a274783cf6007
Daniel201904 <danivar763@gmail.com>
2024-11-29 17:18:07 -0500

    Reparacion de contenido

--- obj/Release/net7.0/E-Commerce.AssemblyInfo.cs
+++ obj/Release/net7.0/E-Commerce.AssemblyInfo.cs
@@ -12,9 +12,9 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Commerce")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb2f86b93921a336b70f6e3c013bcd3e6a58e4d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b752f389224162fae4629589b4e8105cf7d3247")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
